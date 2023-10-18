9ea6fc078647e5e8f840c1bd5f45f681a3b8f0da
Владимир <52661053+VladimirKrutko@users.noreply.github.com>
2023-10-18 18:13:35 +0200

    Create function_1.py
    Pierwsza pseudo funkcja

---
+++ Optymalizacja1/function_1.py
@@ -0,0 +1,27 @@
+def golden_section_search(f, x0, d, alpha, Nmax):
+    x = [x0, x0 + d]
+    fcalls = 2
+    
+    if f(x[1]) == f(x[0]):
+        return x
+    
+    if f(x[1]) > f(x[0]):
+        d = -d
+        x[1] = x[0] + d
+        
+        if f(x[1]) >= f(x[0]):
+            return [x[1], x[0] - d]
+    
+    i = 0
+    while fcalls <= Nmax:
+        i += 1
+        x.append(x[0] + (alpha**i) * d)
+        fcalls += 1
+        
+        if f(x[i]) > f(x[i+1]):
+            break
+    
+    if d > 0:
+        return [x[i-1], x[i+1]]
+    else:
+        return [x[i+1], x[i-1]]
