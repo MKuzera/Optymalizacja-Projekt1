27f490528395e017f29caa9ffa5d3d230e37ab33
MKuzera <mateusz.qzera@gmail.com>
2023-10-20 12:22:40 +0200

    added dane

--- Optymalizacja1/dane.py
+++ Optymalizacja1/dane.py
@@ -9,10 +9,17 @@ class Dane:
     Tb = 10.0 #*C Temp wody zbiornika B
 
     Tbin = 10 #*C Temp wody ktora sie wlewa do zbiornika B
-    Fbin = 10 #*c l/s szybkość wplywania wody do zbiornika B
+    Fbin = 0.01 #*c l/s szybkość wplywania wody do zbiornika B
 
-    Db = 36.5665 #cm^2 prędkość wylewania sie wody z zbiornika B
+    Db = 0.00365665  #m^2 prędkość wylewania sie wody z zbiornika B
 
     a = 0.98 #wspolczynnik odpowiadajacy za lepkosc cieczy
     b = 0.63 #wsp odpowiadajacy za zwężenie strumienia cieczy
     g = 9.81 # przyspieszenie ziemskie
+
+    # szukana: Celem optymalizacji jest znalezienie takiego pola przekroju 𝐷𝐴, dla którego maksymalna temperatura
+    # wody w zbiorniku B będzie równa 50℃ .
+    # Da nalezy [1,100] cm^2
+    t0 = 0 # czas poczatkowy
+    tend = 1000 #s czas koncowy
+    dt = 1 #s skok czasu
