63c3bb328600ee433e79f204c5aa9ab8b6ddc51b
MKuzera <mateusz.qzera@gmail.com>
2023-10-20 12:02:39 +0200

    added info

---
+++ Optymalizacja1/functiondf.py
@@ -0,0 +1,26 @@
+Pa = 0.7 #m^2 Pole podstawy zbiornika A
+Va = 5.0 #m^3 Objetosc zbiornika A
+Ta = 90.0 #*C Temp wody zbiornika A
+
+
+Pb = 1.0 #m^2 Pole podstawy zbiornika B
+Vb = 1.0 #m^3 Objetosc zbiornika B
+Tb = 10.0 #*C Temp wody zbiornika B
+
+Tbin = 10 #*C Temp wody ktora sie wlewa do zbiornika B
+Fbin = 10 #*c l/s szybkość wplywania wody do zbiornika B
+
+Db = 36.5665 #cm^2 prędkość wylewania sie wody z zbiornika B
+
+a = 0.98 #wspolczynnik odpowiadajacy za lepkosc cieczy
+b = 0.63 #wsp odpowiadajacy za zwężenie strumienia cieczy
+g = 9.81 # przyspieszenie ziemskie
+
+#Da - pole przekroju otworu przez które leje sie woda z zbiornika A do B
+
+
+
+def functiondf():
+
+
+    return 0
